dbe06d05a803188167407ab532194f732e1cfd02
ymjoz <rossiblack@duck.com>
2024-10-01 09:49:41 +0800

    fix:透過nginx.conf配置先處理重定向問題

--- image/frontend/layouts/default.vue
+++ image/frontend/layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <p>所有vue共享.0.1.0</p>
+    <p>所有vue共享.0.1.2</p>
     <p>{{ title }}</p>
+    <p>tromso time: {{ tromsoTime }}</p>
     <p>API URL: {{ apiHost }}</p>
     <slot />
   </div>
@@ -14,10 +15,18 @@
     data() {
       const config = useRuntimeConfig()  // 获取 runtimeConfig
       return {
-        title: 'doitt.white',
+        title: 'doitt.white 山陀兒',
         // apiHost: config.public.VUE_APP_API_HOST,
         apiHost: config.public.apiURL,  // 从 runtimeConfig 中获取 apiHost
         // apiHost: process.env.VUE_APP_API_HOST,  // 从环境变量中获取 apiHost
+
+        tromsoTime: new Date().toLocaleString('en-US', {
+          timeZone: 'Europe/Oslo',
+          hour12: false,
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric'
+        })
       }
     },
 
