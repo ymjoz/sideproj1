978049a347f573b29e97d6f5aeb8423c7247ed9c
ymjoz <rossiblack@duck.com>
2024-10-13 22:57:55 +0800

    chore: some stuff

--- image/frontend/layouts/default.vue
+++ image/frontend/layouts/default.vue
@@ -1,51 +1,21 @@
 <template>
   <div>
-    <h2>v2. TROMSO {{ tromsoLocalTime }}</h2>
-    <p>{{ title }}</p>
-    <h3>API URL: {{ apiHost }}</h3>
+    <AppHeader />
     <slot />
+    <AppFooter />
   </div>
 </template>
 
 <script setup>
-  import { ref, computed, onMounted } from 'vue'
+  import { ref } from 'vue'
   import { useRuntimeConfig } from '#app'
 
-  // 获取 runtimeConfig
+  //  runtimeConfig
   const config = useRuntimeConfig()
 
-  // 定义响应式变量
+  // 
   const title = ref('DOITT.WHITE 山陀兒v4')
   const apiHost = ref(config.public.apiURL)
-  const tromsoTime = ref(new Date().toLocaleString('en-US', {
-    timeZone: 'Europe/Oslo',
-    hour12: false,
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-  }))
   const envAll = ref(config.public.envAll)
 
-  // 计算属性
-  const tromsoLocalTime = computed(() => tromsoTime.value)
-
-  // 更新时间的方法
-  const updateTime = () => {
-    tromsoTime.value = new Date().toLocaleString('en-US', {
-      timeZone: 'Europe/Oslo',
-      hour12: false,
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-  }
-
-  // 在组件挂载时启动定时器
-  onMounted(() => {
-    updateTime()
-    setInterval(updateTime, 1000)
-    console.log('test apiHost:', apiHost.value)
-
-    console.log('envAll:', envAll.value)
-  })
 </script>
--- image/frontend/pages/index.vue
+++ image/frontend/pages/index.vue
@@ -1,31 +1,14 @@
 <template>
   <div>
-    <p>
-      {{ title }}
-    </p>
-    <ul>
-      <li>
-        <NuxtLink to="/hello/where">where</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/white/newtest">New Test</NuxtLink>
-      </li>
-      <li>
-        <a href="https://nuxt.com.cn/" target="_blank">NUXT3</a>
-      </li>
-    </ul>
-    <div>
-      <label for="apiResponse">API Response:</label>
-      <pre id="apiResponse">{{ apiResonse }}</pre>
-
-      <!-- <input type="text" id="apiResponse" v-model="apiResponse" readonly /> -->
-    </div>
+    <h1>DOITT WHITE -
+      {{ tromsoLocalTime }}
+    </h1>
   </div>
 </template>
 
 <script setup>
 
-  import { ref, onMounted } from 'vue';
+  import { ref, onMounted, computed } from 'vue';
   import axios from 'axios';
   import { useRuntimeConfig } from '#app';
 
@@ -35,6 +18,34 @@
   const apiHost = ref(config.public.apiURL);
   console.log('hello apiHost:', apiHost.value);
   const apiResonse = ref('xxxxx-xxxxx');
+  const tromsoTime = ref(new Date().toLocaleString('en-US', {
+    timeZone: 'Europe/Oslo',
+    hour12: false,
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  }))
+
+  // 計算属性
+  const tromsoLocalTime = computed(() => tromsoTime.value)
+
+  // 更新時間的方法
+  const updateTime = () => {
+    tromsoTime.value = new Date().toLocaleString('en-US', {
+      timeZone: 'Europe/Oslo',
+      hour12: false,
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+  }
+
+  // 在组件挂载时启动定时器
+  onMounted(() => {
+    updateTime()
+    setInterval(updateTime, 1000)
+
+  })
 
   onMounted(async () => {
     try {
